e4784888ac47e39221ecbf1074f6bb94d851dfcd
Andrey Suvalov <suvalov.andrew@gmail.com>
2021-01-22 15:28:42 +0300

    AwesomeWM: Update volume widget after mouse events

--- .config/awesome/bar.lua
+++ .config/awesome/bar.lua
@@ -18,8 +18,9 @@ local hrz = wibox.layout.align.horizontal
 local arrow0 = arrow_left(color1, color0)
 local arrow1 = arrow_left(color0, color1)
 
+
+local bin = os.getenv("HOME") .. "/.local/bin/"
 local function watch_widget_factory(args)
-  local bin = os.getenv("HOME") .. "/.local/bin/"
   return awful.widget.watch(
     bin .. args.cmd, args.timeout or 5,
     function(widget, stdout)
@@ -39,10 +40,9 @@ local kernel = wibox.widget{
   widget = wibox.widget.textbox
 }
 
-awful.spawn.easy_async_with_shell(os.getenv("HOME") .. "/.local/bin/kernel",
+awful.spawn.easy_async_with_shell(bin .. "kernel",
   function(out)
-    kernel:set_markup_silently(out)
-    kernel.text = out
+    kernel:set_text(out)
   end
 )
 
@@ -68,12 +68,6 @@ backlight:buttons(awful.util.table.join(
   end)
 ))
 
--- KEYBOARD LAYOUT
-local keybrd = awful.widget.keyboardlayout {
-  font = beautiful.font,
-  pattern = "⌨️ %s"
-}
-
 -- volume
 local vol_cmd = os.getenv("HOME") .. "/.local/bin/volume"
 local vol = awful.widget.watch(vol_cmd, 1,
@@ -82,12 +76,23 @@ local vol = awful.widget.watch(vol_cmd, 1,
   end
 )
 
+local function update_volume(args)
+  awful.spawn.easy_async_with_shell(vol_cmd,
+    function(out)
+      vol:set_text(out)
+    end)
+end
+
 vol:buttons(awful.util.table.join(
-  awful.button({}, 4, function() -- scroll up
+  awful.button({}, 4,
+  function() -- scroll up
     os.execute(vol_cmd .. " -s 1%+")
+    update_volume()
   end),
-  awful.button({}, 5, function() -- scroll down
+  awful.button({}, 5,
+  function() -- scroll down
     os.execute(vol_cmd .. " -s 1%-")
+    update_volume()
   end)
 ))
 
@@ -101,10 +106,15 @@ local cpu = osmium.widget.cpu {
     end
 }
 
--- CAL
-local cal = osmium.widget.cal {
+-- datatime with calendar
+local datetime = watch_widget_factory{cmd="datetime",timeout=60}
+osmium.widget.cal{
   attach_to = { datetime },
-  notification_preset = { fg = beautiful.fg_normal, bg = beautiful.bg_normal, font = beautiful.font }
+  notification_preset = {
+    fg = beautiful.fg_normal,
+    bg = beautiful.bg_normal,
+    font = beautiful.font
+  }
 }
 
 local function factory(args)
@@ -126,7 +136,7 @@ local function factory(args)
 
   local create = watch_widget_factory
 
-  push_widget({widget=keybrd})
+  push_widget({widget=awful.widget.keyboardlayout{pattern = "⌨️ %s"}})
   push_widget({widget=wibox.widget {cpuicon, cpu.widget, layout = hrz}})
   push_widget({widget=create{cmd="cpu_temp",valign='top'}})
   push_widget({widget=create{cmd="ram",timeout=2,valign ='top'}})
@@ -136,7 +146,7 @@ local function factory(args)
 --push_widget({widget=backlight})
   push_widget({widget=kernel})
   push_widget({widget=create{cmd="weather",timeout=600}})
-  push_widget({widget=create{cmd="datetime",timeout=60}})
+  push_widget({widget=datetime})
   push_widget({widget=wibox.widget{args.screen.mylayoutbox, layout = hrz},last=true})
   return widgets
 end
