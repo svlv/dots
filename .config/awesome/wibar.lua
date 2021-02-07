12fcc36e5e6faacdbafe6e83c759c9235a84896e
Andrey Suvalov <suvalov.andrew@gmail.com>
2021-02-07 22:02:21 +0300

    Awesome: added wifi widget

--- .config/awesome/wibar.lua
+++ .config/awesome/wibar.lua
@@ -140,6 +140,7 @@ wibar.bar = function (args)
   push_widget{widget=kernel}
   push_widget{widget=create{cmd="weather",timeout=600}}
   push_widget{widget=create{cmd="nettraf",timeout=1}}
+  push_widget{widget=create{cmd="internet",timeout=1}}
   push_widget{widget=datetime}
   push_widget{widget=wibox.widget{args.screen.mylayoutbox, layout = hrz},last=true}
   return widgets
