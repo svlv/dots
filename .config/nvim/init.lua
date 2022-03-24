6be000478011e2adcccaaa13e52d1cc66c795ade
Andrey Suvalov <suvalov.andrew@gmail.com>
2022-03-24 11:15:24 +0100

    Replace vim config with lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,44 @@
+home_dir = os.getenv("HOME") 
+
+vim.o.mouse = 'a'
+
+vim.go.backup = true
+vim.go.backupdir = home_dir .. '/.config/nvim/backup'
+vim.go.dir = home_dir .. '/.config/nvim/tmp'
+
+vim.wo.relativenumber = true
+vim.wo.cursorline = true
+--vim.wo.list = true
+vim.wo.listchars = 'space:·'
+vim.wo.colorcolumn = '80'
+
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.expandtab = true
+vim.bo.autoindent = true
+
+--vim.bo.textwidth = 80
+--set hidden
+
+require('plugins')
+
+vim.cmd([[colorscheme gruvbox]])
+
+-- Airline
+vim.g.airline_theme = 'violet'
+vim.cmd([[let g:airline#extensions#tabline#enabled = 1]])
+vim.cmd([[let g:airline#extensions#tabline#fnamemod = ':t']])
+
+vim.api.nvim_set_keymap('n', '<leader>l', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>h', ':bprevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>p', ':CtrlP<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeFocus<CR>', { noremap = true, silent = true })
+
+vim.g.ctrlp_working_path_mode = 'r'
+vim.cmd([[
+let g:ctrlp_custom_ignore = {
+  \ 'dir':  '\v[\/](\.(git|hg|svn)|\_site)$',
+  \ 'file': '\v\.(so|png|jpg|jpeg)$',
+\}
+]])
+
